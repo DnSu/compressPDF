ab870aabd72b7f7c7cdc763077a323b4f5b63394
Daniel Su <email@danielsu.com>
2019-04-03 15:53:07 -0400

    feat: age and clean up comments

--- compress.php
+++ compress.php
@@ -4,22 +4,26 @@ if ($rcFolder) $srcFolder = './source';
 if ($backupFolder) $backupFolder = './backup';
 if ($age) $age = 365;
 
-compress($srcFolder, $backupFolder);
+compress($srcFolder, $backupFolder, $age);
 
 function compress($srcFolder, $backupFolder, $age) {
-  //echo $srcFolder."\n";
+  $ageLimit = 86400 * $age; // 5 days
+
   $files = scandir($srcFolder);
-  // print_r($files);
-  $ageLimit = 86400 * 0; // 5 days
   $ago = time() - filemtime($srcFolder);
 
   if ($ago > $ageLimit) {
     foreach($files as $file) {
       if ($file == '.' || $file == '..') continue;
       if(is_dir($srcFolder.'/'.$file)){
+        
+        // go down one level
         if(!is_dir($backupFolder.'/'.$file)) mkdir($backupFolder.'/'.$file);
         compress($srcFolder.'/'.$file, $backupFolder.'/'.$file, $age);
+
       } else {
+
+        // compress file
         if(!is_file($backupFolder.'/'.$file)){
           if(preg_match('/(pdf|PDF)$/', $file)) {
             $dotPos = strrpos($srcFolder.'/'.$file, '.');
@@ -28,14 +32,19 @@ function compress($srcFolder, $backupFolder, $age) {
             $tmpName = substr($srcName, 0, $dotPos);
             $tmpName .= '_bk';
             $tmpName .= substr($srcName, $dotPos);
-            // echo $tmpName."\n";
-            // echo $srcFolder.'/'.$file."\n";
             $cmd = 'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.6 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="'.$srcName.'" "'.$tmpName.'"';
-            // echo $cmd."\n";
+
+            // make a backup copy
             copy($srcName, $backupName );
+
             if (is_file($backupName)) {
+              // move original file to temp file
               rename($srcName, $tmpName);
+
+              // compress the actual file
               system($cmd);
+
+              // delete the temp file
               unlink($tmpName);
             }
           }
